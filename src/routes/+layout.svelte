27d613a207760f92702169b79d0635efdf8093dd
Iris Veress <irisveress@gmail.com>
2024-12-13 16:45:10 +0200

    init commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,8 @@
+<script>
+	import '../app.css';
+	let { children } = $props();
+</script>
+
+<div class="flex justify-center items-center flex-col my-8 mx-20">
+	{@render children()}
+</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,2 @@
+<h1>Generate a QR code for OCF</h1>
+<p>See the following</p>
